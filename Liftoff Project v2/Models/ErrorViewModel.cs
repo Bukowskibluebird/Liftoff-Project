0243f7fbe6b6478e6f6f44b805c6b7b9c44bfe90
Ryan <hayes.ryan95@gmail.com>
2019-01-17 11:39:42 -0600

    Add project files.

---
+++ Liftoff Project v2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Liftoff_Project_v2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
